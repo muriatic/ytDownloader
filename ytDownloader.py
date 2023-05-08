123a644419640530ca54f6fc857194226d71f73e
muriatic <64611874+muriatic@users.noreply.github.com>
2023-05-08 18:39:31 -0400

    Update ytDownloader.py
    1. Fixed FileNotFoundError not providing the file that wasn't found
    2. Added cases for capital letters
    3. Reduced the number of function calls to once per second when listening for key input
    4. Added .mp4 to .wav and downloading .wav file features

--- ytDownloader.py
+++ ytDownloader.py
@@ -2,6 +2,8 @@ from pytube import YouTube
 import os
 from moviepy.editor import *
 import keyboard
+from time import sleep
+
 
 def download_video(video_url, name):
     nameMP4 = name + ".mp4"
@@ -15,107 +17,167 @@ def download_video(video_url, name):
 
     print("Video Downloaded Successfully")
 
+
 dir_path = os.getcwd() # os.path.dirname(os.path.realpath(__file__))
 
-def convertMP4ToMP3(name):
+
+def convertMP4(name, type):
+
     nameMP4 = name + ".mp4"
-    nameMP3 = name + ".mp3"
+    
+    print(nameMP4)
+
+    match type:
+        case 'mp3':
+            endName = name + ".mp3"
+        case 'wav':
+            endName = name + '.wav'
 
     video_file_path = os.path.join(dir_path, nameMP4)
-    audio_file_path = os.path.join(dir_path, nameMP3)
+    audio_file_path = os.path.join(dir_path, endName)
 
     if os.path.exists(video_file_path):
+
         FILETOCONVERT = AudioFileClip(video_file_path)
         FILETOCONVERT.write_audiofile(audio_file_path)
 
         FILETOCONVERT.close()
-
-        return nameMP4
     
     else:
-        raise FileNotFoundError
+        raise FileNotFoundError(f"FileNotFoundError: file {nameMP4} not found")
 
 # def conversion(name):
 #     print("What file would you ")
 
-def question():
+def mp3ORmp4(name, link=''):
+    key_event = keyboard.read_event(suppress=True)
+
+    formatQuestion = key_event.name
+
+    nameMP4 = name + '.mp4'
+
+    sleep(1)
+
+    match formatQuestion:
+        case '0':
+            if link != '':
+                download_video(link, name)
+
+            convertMP4(name, 'mp3')
+        
+        case '1':
+            if link != '':
+                download_video(link, name)
+
+            convertMP4(name, 'wav')
+
+        case 'esc':
+            quit()
+
+        case _:
+            mp3ORmp4(name, link)
+
+    if os.path.exists(nameMP4):
+        os.remove(nameMP4)
+
+
+def yes1():
+    print("Files in Directory:")
+
+    # get list of Files
+    listOfFiles = []
+
+    n = 0
+
+    for file in os.listdir():
+        if file.endswith(".mp4"):
+            print(f"({n}) {file}")
+            listOfFiles.append(file)
+            n += 1
+
+    # get name or position
+    name = str(input("\nFile Name: \n>>> "))
+
+    # try to convert to Integer
+    try:
+        position = int(name)
+        file = listOfFiles[position].removesuffix('.mp4')
+
+        print("\nFile Format: \n(0) .mp3 \n(1) .wav\n")
+
+        mp3ORmp4(file)
+
+    except ValueError:
+        try:
+            if name.endswith('.mp4'):
+                name = name.removesuffix('.mp4')
+
+            mp3ORmp4(name)
+            
+        except FileNotFoundError:
+            print(f"FileNotFoundError: file {name}.mp4 does not exist")
+
+
+def no1():
+    link = str(input("Video URL: \n>>> "))
+    name = str(input("File Name: \n>>> "))
+
+    while True:    
+        audioQuestion = str(input("Audio Only: \n>>> ")).lower()
+
+        if audioQuestion == "yes":
+            
+            print("File Format: \n(0) .mp3 \n(1) .wav\n")
+
+            mp3ORmp4(name, link)
+
+            break
+
+        elif audioQuestion == "no":
+            download_video(link, name)
+            break
+
+        else:
+            continue
+
+def question1():
     key_event = keyboard.read_event(suppress=True)
     
     MP4ToMP3Question = key_event.name
-
+    
+    sleep(1)
 
     match MP4ToMP3Question:
         case 'y':
-            print("Files in Directory:")
+            yes1()
 
+        case 'Y':
+            yes1()
 
-            # get list of Files
-            listOfFiles = []
-
-            n = 0
-
-            for file in os.listdir():
-                if file.endswith(".mp4"):
-                    print(f"({n}) {file}")
-                    listOfFiles.append(file)
-                    n += 1
-
-            # get name or position
-            name = str(input("\nFile Name: \n>>> "))
-
-            # try to convert to Integer
-            try:
-                position = int(name)
-                file = listOfFiles[position].removesuffix('.mp4')
-                convertMP4ToMP3(file)
-
-            except ValueError:
-                try:
-                    if name.endswith('.mp4'):
-                        name = name.removesuffix('.mp4')
-                        convertMP4ToMP3(name)
-                    else:
-                        convertMP4ToMP3(name)
-                    
-                except FileNotFoundError:
-                    print(f"FileNotFoundError: file {name}.mp4 does not exist")
-            
+        case '0':
+            yes1()
         
         case 'n':
-            link = str(input("Video URL: \n>>> "))
-            name = str(input("File Name: \n>>> "))
+            no1()
 
-            while True:    
-                audioQuestion = str(input("Audio Only: \n>>> ")).lower()
-
-                if audioQuestion == "yes":
-                    download_video(link, name)
-                    nameMP4 = str(convertMP4ToMP3(name))
-
-                    if os.path.exists(nameMP4):
-                        os.remove(nameMP4)
-
-                    break
-
-                elif audioQuestion == "no":
-                    download_video(link, name)
-                    break
-
-                else:
-                    continue
+        case 'N':
+            no1()
+        
+        case '1':
+            no1()
         
         case 'esc':
-            return 0
+            quit()
 
         case _:
-            question()
+            question1()
 
 def main():
     print("Press ESC at anytime to exit...\n")
 
-    print("Would you like to use MP4 to MP3? \n1. (Y)\n2. (N)\n")
+    print("Would you like to use MP4 to MP3? \n0. (Y)\n1. (N)\n")
     
-    question()
+    question1()
 
     input("\nPress enter to close...")
 
