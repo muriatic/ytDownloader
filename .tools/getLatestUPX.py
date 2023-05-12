0393ab1dd13167813a8df02abfacc345f1e950ef
muriatic <64611874+muriatic@users.noreply.github.com>
2023-05-11 23:05:28 -0400

    Created getLatestUPX.py
    - gets the latest release of windows x64 UPX from github, extracts it and sends the contents to the .venv/upx folder.
    - UPX could very well get retired based on reports that it causes startup performance issues

---
+++ .tools/getLatestUPX.py
@@ -0,0 +1,43 @@
+import requests
+import zipfile
+import os
+import urllib.request
+import shutil
+
+UPXLink = "https://api.github.com/repos/upx/upx/releases/latest"
+
+response = requests.get(UPXLink)
+
+body = response.json()["body"]
+
+UPXFolder = '.venv/upx/'
+
+windowsInstaller = body.split('UPX - X64 Win64 version')[0]
+windowsInstaller = windowsInstaller.rsplit('|', 2)[1]
+windowsInstaller = windowsInstaller.split('(')[1]
+windowsInstallerLink = windowsInstaller.split(')')[0]
+
+# folder name
+folderName = windowsInstallerLink.rsplit('/', 1)[1]
+folderName = folderName.removesuffix('.zip')
+
+zip_path, _ = urllib.request.urlretrieve(windowsInstallerLink)
+zipData = zipfile.ZipFile(zip_path)
+
+with zipfile.ZipFile(zip_path, "r") as f:
+    f.extractall()
+
+files = [f for f in os.listdir(folderName)]
+
+for file in files:
+    shutil.move(folderName + '/' + file, UPXFolder + file)
+
+try:
+    shutil.rmtree(folderName)
+    # os.remove
+except OSError:
+    pass
+
+
+# if os.path.isfile("upx.zip"):
+#     os.remove("upx.zip")
--- getLatestUPX.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-UPXLink = "https://api.github.com/repos/upx/upx/releases/latest"
-
-response = requests.get(UPXLink)
-
-print(response.json()["assets"])
